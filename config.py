ef788f8da8b36a5178f93eba8a645a3721890df7
Ankit kumar <ankitgupta2ndsp@gmail.com>
2025-07-24 23:32:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Loads values from .env if running locally
+load_dotenv()  # Only used in local environment (Heroku sets env automatically)
 
 API_ID = int(os.getenv("API_ID", "123456"))
 API_HASH = os.getenv("API_HASH", "your_api_hash")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
-FORCE_JOIN = os.getenv("FORCE_JOIN", "https://t.me/yourchannel")
+FORCE_JOIN = os.getenv("FORCE_JOIN", "")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001234567890"))
 
-MONGO_URI = os.getenv("MONGO_URI", "your_mongo_uri")
+MONGO_URI = os.getenv("MONGO_URI", "")
 MONGO_DB_NAME = "devil_osint"
